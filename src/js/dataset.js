5eed9d07128b721f771f608635a3d34c95c2d4ab
khyatikava <93000239+khyatikava@users.noreply.github.com>
2022-02-27 17:04:48 -0500

    code for frontend task bug solves

---
+++ src/js/dataset.js
@@ -0,0 +1,149 @@
+var reportData = [
+    {
+        "cover": "images/cover1.jpg",
+        "type": "Annual Report",
+        "title": "2019 Annual Report",
+        "documents": [
+            {
+                "file_size": "16 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2019 Annual Report",
+				"subtitle":"(16 KB Pdf)"
+            },
+            {
+                "file_size": "7 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2019 Proxy Statement",
+				"subtitle":"(7 KB Pdf)"
+            }
+        ]
+    },
+    {
+        "cover": "images/cover2.jpg",
+        "type": "Annual Report",
+        "title": "2018 Annual Report",
+        "documents": [
+            {
+                "file_size": "16 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2018 Annual Report",
+				"subtitle":"(16 KB Pdf)"
+            },
+            {
+                "file_size": "7 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2018 Proxy Statement",
+				"subtitle":"(7 KB Pdf)"
+            }
+        ]
+    },
+    {
+        "cover": "images/cover3.jpg",
+        "type": "Annual Report",
+        "title": "2017 Annual Report",
+        "documents": [
+            {
+                "file_size": "16 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2017 Annual Report",
+				"subtitle":"(16 KB Pdf)"
+            },
+            {
+                "file_size": "7 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2017 Proxy Statement",
+				"subtitle":"(7 KB Pdf)"
+            }
+        ]
+    },
+    {
+        "cover": "images/cover1.jpg",
+        "type": "Annual Report",
+        "title": "2016 Annual Report",
+        "documents": [
+            {
+                "file_size": "16 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2016 Annual Report",
+				"subtitle":"(16 KB Pdf)"
+            },
+            {
+                "file_size": "7 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2016 Proxy Statement",
+				"subtitle":"(7 KB Pdf)"
+            }
+        ]
+    },
+    {
+        "cover": "images/cover2.jpg",
+        "type": "Annual Report",
+        "title": "2015 Annual Report",
+        "documents": [
+            {
+                "file_size": "16 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2015 Annual Report",
+				"subtitle":"(16 KB Pdf)"
+            },
+            {
+                "file_size": "7 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2015 Proxy Statement",
+				"subtitle":"(7 KB Pdf)"
+            }
+        ]
+    },
+    {
+        "cover": "images/cover3.jpg",
+        "type": "Annual Report",
+        "title": "2014 Annual Report",
+        "documents": [
+            {
+                "file_size": "16 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2014 Annual Report",
+				"subtitle":"(16 KB Pdf)"
+            },
+            {
+                "file_size": "7 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2014 Proxy Statement",
+				"subtitle":"(7 KB Pdf)"
+            }
+        ]
+    },
+    {
+        "cover": "images/cover1.jpg",
+        "type": "Annual Report",
+        "title": "2013 Annual Report",
+        "documents": [
+            {
+                "file_size": "16 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2013 Annual Report",
+				"subtitle":"(16 KB Pdf)"
+            },
+            {
+                "file_size": "7 KB",
+                "file_type": "PDF",
+                "url": "files/test.pdf",
+                "title": "2013 Proxy Statement",
+				"subtitle":"(7 KB Pdf)"
+            }
+        ]
+    }
+];
